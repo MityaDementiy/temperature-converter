984e7ddbb21ea83c07120e9cdb27f3b487e3a392
MityaDementiy <d.dementiy@gmail.com>
2022-10-07 18:31:16 +0300

    Add clear button

--- src/learn_cljs/temp_converter.cljs
+++ src/learn_cljs/temp_converter.cljs
@@ -2,10 +2,10 @@
   (:require [goog.dom :as gdom]
             [goog.events :as gevents]))
 
-(defn f->c [deg-f] 
+(defn f->c [deg-f]
   (/ (- deg-f 32) 1.8))
 
-(defn c->f [deg-c] 
+(defn c->f [deg-c]
   (+ (* deg-c 1.8) 32))
 
 (def celsius-radio (gdom/getElement "unit-c"))
@@ -13,8 +13,9 @@
 (def temp-input (gdom/getElement "temp"))
 (def output-target (gdom/getElement "temp-out"))
 (def output-unit-target (gdom/getElement "unit-out"))
+(def clear-button (gdom/getElement "clear-input"))
 
-(defn get-input-unit [] 
+(defn get-input-unit []
   (if (.-checked celsius-radio)
     :celsius
     :fahrenheit))
@@ -22,11 +23,13 @@
 (defn get-input-temp []
   (js/parseInt (.-value temp-input)))
 
-(defn set-output-temp [temp] 
+(defn set-output-temp [temp]
   (gdom/setTextContent output-target (.toFixed temp 2)))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(defn clear-input [] 
+  (set! (.-value temp-input) "")
+  (gdom/setTextContent output-target "-"))
+
 
 (defn update-output [_]
   (if (= :celsius (get-input-unit))
@@ -38,10 +41,11 @@
 (gevents/listen temp-input "keyup" update-output)
 (gevents/listen celsius-radio "click" update-output)
 (gevents/listen fahrenheit-radio "click" update-output)
+(gevents/listen clear-button "click" clear-input)
 
 ;; specify reload hook with ^:after-load metadata
 (defn ^:after-load on-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
